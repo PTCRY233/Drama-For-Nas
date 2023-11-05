a7f8c6c85111de2c147cb884baab38b1408f57e7
PTCRY <55911222+PTCRY233@users.noreply.github.com>
2023-11-05 12:30:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ONLINE_VERSION=$(curl -Ls "https://api.github.com/repos/ptcry233/drama-for-nas/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-wget -NO /tmp/setupd.sh https://github.com/ptcry233/drama-for-nas/releases/download/$ONLINE_VERSION/setupd.sh && chmod +x /tmp/setupd.sh && bash /tmp/setupd.sh
+wget -NO /tmp/drama.sh https://github.com/ptcry233/drama-for-nas/releases/download/$ONLINE_VERSION/drama.sh && chmod +x /tmp/drama.sh && bash /tmp/drama.sh
